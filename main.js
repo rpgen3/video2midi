f254d3504506eaec34490013647c9784c391008b
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-11-25 08:01:41 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+(async () => {
+    const {importAll, getScript, importAllSettled} = await import(`https://rpgen3.github.io/mylib/export/import.mjs`);
+    await getScript('https://code.jquery.com/jquery-3.3.1.min.js');
+    const {$} = window;
+    const html = $('body').empty().css({
+        'text-align': 'center',
+        padding: '1em',
+        'user-select': 'none'
+    });
+    const head = $('<header>').appendTo(html),
+          main = $('<main>').appendTo(html),
+          foot = $('<footer>').appendTo(html);
+    $('<h1>').appendTo(head).text('video2midi');
+    const rpgen3 = await importAll([
+        'input',
+        'css',
+        'url',
+        'hankaku',
+        'sample',
+        'util'
+    ].map(v => `https://rpgen3.github.io/mylib/export/${v}.mjs`));
+    Promise.all([
+        'container',
+        'table',
+        'kernel',
+        'tab',
+        'img'
+    ].map(v => `css/${v}.css`).map(rpgen3.addCSS));
+    $('<h2>').appendTo(head).text('動画の読み込み');
+    const video = new class {
+        constructor(){
+            const html = $('<div>').appendTo(head).addClass('container');
+            $('<h3>').appendTo(html).text('動画を入力');
+            this.input = $('<dl>').appendTo(html);
+            this.output = $('<div>').appendTo(html);
+            this.video = null;
+        }
+        async load(url){
+            $(this.video = await rpgen3.loadSrc('video', url)).appendTo(this.output.empty());
+        }
+    };
+    {
+        const {input} = video;
+        $('<dt>').appendTo(input).text('ファイル入力');
+        $('<input>').appendTo($('<dd>').appendTo(input)).prop({
+            type: 'file'
+        }).on('change', ({target}) => {
+            const {files} = target;
+            if(files.length) video.load(URL.createObjectURL(files[0]));
+        });
+    }
+})();
